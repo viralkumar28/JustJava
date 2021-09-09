adf368db8f7ce19f25e4a95efc039cba1d01af26
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2021-09-09 17:40:46 +0530

    Update second.java

--- second.java
+++ second.java
@@ -1,5 +1,10 @@
+import java.util.*;
 public class second {
     public static void main(String args[]) {
-        
+        Scanner reader = new Scanner (System.in);
+        System.out.println("Enter a number: ");
+        int number = reader.nextInt();
+        System.out.println(number);
+        reader.close();
     }
 }
