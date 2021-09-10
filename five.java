a19d06f23d5b13f6787fe8c197d01fd8d5c93d9f
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2021-09-10 17:12:21 +0530

    Update five.java

--- five.java
+++ five.java
@@ -2,7 +2,9 @@ import java.util.*;
 public class five {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+        char a = 'v';
         
+        System.out.println((int)a);
         sc.close();
     }
 }
