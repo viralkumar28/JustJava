ca3cfd360a00b9ec4719e2095ec2fde13e088db5
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2021-09-10 18:08:47 +0530

    Create eleven.java

---
+++ eleven.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class eleven{
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(Math.sqrt(a));
+        sc.close();
+    }
+
+    
+}
