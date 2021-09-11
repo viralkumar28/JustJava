1afd39cbbd6cc525de4898c58b8ad789d8276a0a
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2021-09-11 17:49:37 +0530

    Update twelve.java

--- twelve.java
+++ twelve.java
@@ -1,5 +1,19 @@
+import java.util.*;
+
 public class twelve {
-    public static void main(String args[]) {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        for(String word : s.split(" ")){
+            if(word.length() % 2 == 0){
+                System.out.println(word);
+            }
+                
+        }
         
+        scan.close();
     }
 }
+
