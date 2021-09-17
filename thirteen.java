40d06d3625b91cb47bd4c7e4682f762d4ebf3bb5
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2021-09-17 22:47:51 +0530

    Update thirteen.java

--- thirteen.java
+++ thirteen.java
@@ -1,5 +1,14 @@
+import java.util.*;
 public class thirteen {
     public static void main(String args[]){
-
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        if(year%4==0){
+            System.out.println("Leap year");
+        }
+        else{
+            System.out.println("Not leap year");
+        }
+        sc.close();
     }
 }
