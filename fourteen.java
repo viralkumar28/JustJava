f727668f81c16cdbf9f136fb871c55ea25e43b10
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2021-09-18 21:10:45 +0530

    Update fourteen.java

--- fourteen.java
+++ fourteen.java
@@ -3,7 +3,18 @@ import java.util.Scanner;
 public class fourteen {
     public static void main(String args[]) {
        Scanner sc = new Scanner(System.in);
-
+       float a = sc.nextFloat(); 
+       if(a>0.0){
+           System.out.println("Positive");
+       }else if(a<0.0){
+           System.out.println("Negative");
+       }
+       else if(a == 0.0){
+           System.out.println("Zero");
+       }
+       else{
+           System.out.println("Special Character");
+       }
        sc.close();
       
        
