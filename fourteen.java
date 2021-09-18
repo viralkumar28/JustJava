10f88a2a0a4ddb48e4e97a87b9f6ed7995b49125
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2021-09-18 21:08:28 +0530

    Update fourteen.java

--- fourteen.java
+++ fourteen.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class fourteen {
     public static void main(String args[]) {
        Scanner sc = new Scanner(System.in);
+
+       sc.close();
       
        
     }
