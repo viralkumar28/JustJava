7ca970da3b4e2bea96982ad0a77d3348572f3dcf
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2021-09-19 18:11:53 +0530

    Update fifteen.java

--- fifteen.java
+++ fifteen.java
@@ -1,5 +1,13 @@
+import java.util.*;
 public class fifteen {
     public static void main(String args[]) {
-        
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int sum =0;
+        for(int i=0;i<=a;i++){
+            sum += i;
+        }
+        System.out.println("Sum = " +sum);
+        sc.close();
     }
 }
