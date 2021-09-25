af85da0aa48e7891a1cb3a461d10575ccc49e46c
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2021-09-25 18:58:11 +0530

    Update sixteen.java

--- sixteen.java
+++ sixteen.java
@@ -2,13 +2,15 @@ import java.util.*;
 // import java.lang.*;
 // import java.io.*;
 public class sixteen{
-    public static void name(String[] args)throws java.lang.Exception {
+    public static void main(String[] args)throws java.lang.Exception {
         Scanner sc = new Scanner(System.in);
+        int a=0;
 		int marks[] = new int[3];
-		for(int i=0;i<= marks.length;i++){
-		    int a = sc.nextInt();
-            System.out.println(a);
+		for(int i=1;i<= marks.length;i++){
+		     a = sc.nextInt();
 		}
+        for(int )
+        System.out.println(a);
         sc.close();
     }
 }
