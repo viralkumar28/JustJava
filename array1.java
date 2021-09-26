a45ab7851c98bacf8fd69ac92d786a6a0b7867b8
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2021-09-26 18:04:20 +0530

    Create array1.java

---
+++ array1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class array1 {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int q = sc.nextInt();
+        int N[] = new int[n];
+        int Q[] = new int[q];
+		for(int i=0;i<N.length;i++){
+		     N[i] = sc.nextInt();
+		}
+		for(int i=0;i<Q.length;i++){
+		    for(int j=0;j<3;j++){
+		         Q[j] = sc.nextInt();
+		    }
+		}
+        sc.close();
+	}
+}
