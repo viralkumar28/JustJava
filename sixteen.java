122452def5d4f86acbb47973bb3922632716845b
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2021-09-26 15:34:22 +0530

    Update sixteen.java

--- sixteen.java
+++ sixteen.java
@@ -4,13 +4,30 @@ import java.util.*;
 public class sixteen{
     public static void main(String[] args)throws java.lang.Exception {
         Scanner sc = new Scanner(System.in);
-        int a=0;
+        
 		int marks[] = new int[3];
-		for(int i=1;i<= marks.length;i++){
-		     a = sc.nextInt();
+		for(int i=0;i< marks.length;i++){
+            marks[i] = sc.nextInt();
 		}
-        for(int )
-        System.out.println(a);
+        int secondLargest = findSecondLargest(marks);
+		System.out.println(secondLargest);
         sc.close();
-    }
+	}
+	public static int findSecondLargest(int array[]){
+
+        int highest = Integer.MIN_VALUE;
+        int secondHighest = Integer.MIN_VALUE;
+        for(int i=0; i<array.length;i++){
+            if(array[i]>=highest){
+                secondHighest = highest;
+                highest = array[i];
+            }else if(array[i] >= secondHighest && array[i]!=highest){
+                secondHighest = array[i];
+            }
+        }
+
+        return secondHighest;
+	    
+	}
+   
 }
