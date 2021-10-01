9e45cd4bd5ace4ce89ab8aba8eaa9372527efc14
viralkumar28 <viralkumarmaurya@gmail.com>
2021-10-01 15:36:09 +0530

    Update concatinate.java

--- concatinate.java
+++ concatinate.java
@@ -2,16 +2,12 @@ import java.util.*;
 
 class Solution {
     public int[] getConcatenation(int[] nums) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        nums = new int[n];
-            for(int i=0;i<n;i++){
-                 n = sc.nextInt();
-            }
-        for(int j = 0;j<n;j++){
-            System.out.println();    
+        int l = nums.length;
+        int[] ans = new int[2*l];
+        for(int i=0;i<l;i++){
+            ans[i] = nums[i];
+            ans[i+l] = nums[i];
         }
-        return nums;
-        
+        return ans;   
     }
 }
