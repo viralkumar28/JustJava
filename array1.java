8d84224318eb8cbd59c17e78ff76a0226b97b3fe
viralkumar28 <viralkumarmaurya@gmail.com>
2021-10-01 15:27:43 +0530

    new problems

--- array1.java
+++ array1.java
@@ -1,6 +1,5 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
+
 public class array1 {
     public static void main (String[] args) throws java.lang.Exception
 	{
---
+++ concatinate.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        nums = new int[n];
+            for(int i=0;i<n;i++){
+                 n = sc.nextInt();
+            }
+        for(int j = 0;j<n;j++){
+            System.out.println();    
+        }
+        return nums;
+        
+    }
+}
