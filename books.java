e3ceb63a44ff2989e2ebdda33a42cc32516dce84
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2021-10-30 20:51:41 +0530

    commit

---
+++ books.java
@@ -0,0 +1,23 @@
+// A - Books
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class books
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		int N[] = new int[T]; 
+		for (int i=0;i<T ;i++ ){
+		   N[i] = sc.nextInt();
+		   for(int j=0;j<N.length;j++){
+		       int a[] = new int[];
+                
+		   }
+		} 
+	}
+}
