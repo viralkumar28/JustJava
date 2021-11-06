0aa76204b7bd2f96c22cea29794a826410cba3d0
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2021-11-06 15:49:21 +0530

    commit

--- books.java
+++ books.java
@@ -1,14 +1,18 @@
-// A - Books
+
 import java.util.*;
 
-
-/* Name of the class has to be "Main" only if the class is public. */
 class books
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		
+		Scanner sc = new Scanner(System.in);		
+		String sentence = sc.nextLine();
+
+		System.out.println("Oiginal: "+sentence);
+		sentence = sentence.replaceAll("\\s", "");
+		System.out.println("After replacement: " +sentence);
+
+		sc.close();
+
 	}
 }
---
+++ eighteen.java
@@ -0,0 +1,5 @@
+public class eighteen {
+    public static void main(String args[]) {
+        
+    }
+}
