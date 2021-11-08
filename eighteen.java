74fc88361a6a281e3d5e98c6a390feacdb45c134
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2021-11-08 16:00:40 +0530

    commit

--- eighteen.java
+++ eighteen.java
@@ -1,5 +1,21 @@
+import java.util.*;
+
 public class eighteen {
     public static void main(String args[]) {
-        
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.println(isNullEmpty(a));
+
+        sc.close();
+    }
+    public static String isNullEmpty(String a) {
+        if(a == null){
+            return "NULL";
+        }else if(a.isEmpty()){
+            return "Empty";
+        }else{
+            return a;
+        }
+            
     }
 }
