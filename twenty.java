e85a8da6333e16382673c07afb0ad7ffa35f0050
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-01-07 21:41:00 +0530

    commit

--- twenty.java
+++ twenty.java
@@ -2,7 +2,12 @@ import java.util.*;
 public class twenty {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        
+        int a = 0 ;
+        int b = 1;
+        int c = sc.nextInt();
+        for(int i=0;i<c;i++){
+            
+        }
 
         sc.close();
     }
