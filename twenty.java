e4c0fa0ce5f9f6f18dfbe9462134af75f061b507
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-01-07 23:04:34 +0530

    commit

--- twenty.java
+++ twenty.java
@@ -5,9 +5,14 @@ public class twenty {
         int a = 0 ;
         int b = 1;
         int c = sc.nextInt();
+        System.out.print(a+" ");
+        System.out.print(b);
+        int d = a+b;
+        
         for(int i=0;i<c;i++){
-            
+        
         }
+        System.out.println();
 
         sc.close();
     }
