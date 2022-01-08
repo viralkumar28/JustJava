ba03f1d506fa8392373acd916b57d6399eb97302
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-01-08 11:46:14 +0530

    commit

--- twenty.java
+++ twenty.java
@@ -1,17 +1,30 @@
-import java.util.*;
 public class twenty {
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int a = 0 ;
-        int b = 1;
-        int c = sc.nextInt();
-       
-        for(int i=1;i<=c;++i){
-            System.out.print(a+" ");
-            int d = a+b;
-                a=b;
-                b=d;
+        int low = 999, high = 99999;
+
+        for(int number = low + 1; number < high; ++number) {
+          int digits = 0;
+          int result = 0;
+          int originalNumber = number;
+    
+          // number of digits calculation
+          while (originalNumber != 0) {
+            originalNumber /= 10;
+            ++digits;
+          }
+    
+          originalNumber = number;
+    
+          // result contains sum of nth power of its digits
+          while (originalNumber != 0) {
+            int remainder = originalNumber % 10;
+            result += Math.pow(remainder, digits);
+            originalNumber /= 10;
+          }
+    
+          if (result == number) {
+            System.out.print(number + " ");
+          }
         }
-        sc.close();
     }
 }
