2865095218bf5a2d85fc94c80bc7be0c28ec14b3
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-01-08 15:24:22 +0530

    commit

--- twentyone.java
+++ twentyone.java
@@ -1,5 +1,13 @@
+import java.util.*;
 public class twentyone {
     public static void main(String args[]) {
-        
+        Scanner sc = new Scanner(System.in);
+        int input =0;
+        int  n = sc.nextInt();
+        while(n!=0){
+              n = n+ input;
+                
+        }
+        sc.close();
     }
 }
