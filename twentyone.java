f817d2c4b2afa35a8b9378cdcff7e1c8517fabce
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-01-09 13:11:45 +0530

    commit

--- twentyone.java
+++ twentyone.java
@@ -1,11 +1,42 @@
-import java.lang.*;
+ import java.lang.*;
 import java.util.*;
 public class twentyone {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-           int x= sc.nextInt();
-
-           System.out.println(Math.pow(x, 10));   
-        sc.close();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        max(a,b,c);
+        min(a,b,c);
+    }
+    static void max(int a,int b,int c){
+        if(a>b){
+            if(a>c){
+                System.out.println(a);
+            }else{
+                System.out.println(c);
+            }
+        }
+         else if(b>c){
+            System.out.println(b);
+         }
+         else{
+            System.out.println(c);
+         }
+    }
+    static void min(int a, int b,int c){
+        if(a<b){
+            if(a<c){
+                System.out.println(a);
+            }
+            else{
+                System.out.println(c);
+            }
+        }
+        else if(b<c){
+            System.out.println(b);
+        }else{
+            System.out.println(c);
+        }
     }
 }
