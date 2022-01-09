0656e920325242a5c71ad86d8b5e49c51c372bd8
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-01-09 13:16:13 +0530

    commit

---
+++ pr22.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class pr22 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        evenandodd(num);
+        sc.close();
+
+    }
+    static void evenandodd(int num){
+        if(num % 2==0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+    }
+}
