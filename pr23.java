dc64da698a25edd4d618dbb66164e8df33dd76cb
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-01-13 16:50:14 +0530

    counting occurrences

--- pr23.java
+++ pr23.java
@@ -1,5 +1,22 @@
+//counting occurrences
+
+import java.util.*;
+
 public class pr23 {
     public static void main(String args[]) {
-        
+        Scanner sc= new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int n = sc.nextInt();
+        int count=0;
+        while(num > 0){
+            if(num % 10 == n){
+                count++;
+            }
+            num /= 10;
+        }
+        System.out.println(count);
+        sc.close();
+
     }
 }
