0c771be73797277816b2f9d3a3de8d2672a7fef6
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-01-13 17:06:52 +0530

    commit

--- pr24.java
+++ pr24.java
@@ -1,9 +1,17 @@
+//reverse number
 import java.util.*;
 
 public class pr24 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        
+        int num = sc.nextInt();
+        int ans = 0;
+        while(num>0){
+           int rem = num % 10;
+           num /= 10;
+            ans = ans*10+rem ;
+        }
+        System.out.println(ans);
         sc.close();
     }
 }
