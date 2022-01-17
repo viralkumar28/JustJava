dd82fab24032f53a8dd373edd5eb7f270883a561
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-01-17 15:58:59 +0530

    commit

--- pr25.java
+++ pr25.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class pr25 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        String fr = sc.next();
-        switch(fr){
-            case "mango" -> 
-             System.out.println("King of fruits");
-        }
+        // String fr = sc.next();
+        // switch(fr){
+        //     case "mango" -> 
+        //      System.out.println("King of fruits");
+        // }
+
+        
         sc.close();
     }
 }
