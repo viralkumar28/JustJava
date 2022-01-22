cb64e02a16149462d5a17f8c139e6b36e54c0406
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-01-22 16:43:36 +0530

    commit

--- pr26.java
+++ pr26.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 public class pr26 {
     public static void main(String []args) {
         Scanner in = new Scanner(System.in);    
-
-        
+        int age = in.nextInt();
+        voting(age);
         in.close();
     }
-    static void fun(int ...v){
-        
+    static void voting(int age){
+        if(age >= 18){
+            System.out.println("Allowed for voting");
+        }else{
+            System.out.println("Not allowed");
+        }
     }
 }
