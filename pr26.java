f3861b6624fe272919dc30c26d331f2c490efaf5
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-01-22 16:36:54 +0530

    removed array

--- pr26.java
+++ pr26.java
@@ -1,10 +1,9 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class pr26 {
     public static void main(String []args) {
         Scanner in = new Scanner(System.in);    
-        
+
         
         in.close();
     }
