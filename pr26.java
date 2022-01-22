cd4b2c6b186f334036ee0d5eaa796f9750c27b59
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-01-22 16:35:02 +0530

    nothing added

--- pr26.java
+++ pr26.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 public class pr26 {
     public static void main(String []args) {
         Scanner in = new Scanner(System.in);    
-        fun(2,3,4,5,6,1,8,9);
+        
         
         in.close();
     }
     static void fun(int ...v){
-        System.out.println(Arrays.toString(v));
+        
     }
 }
