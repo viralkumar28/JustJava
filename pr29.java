56c60e2495c2437ebeba036079b1de6a8d63925c
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-01-26 14:21:39 +0530

    commit

--- pr29.java
+++ pr29.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class pr29 {
     public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int count = 0;
         
-
+        
+        in.close();
     }
 }
