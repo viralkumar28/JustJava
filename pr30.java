b75cbbe3a45f6aa0a8cbaad8d2fed57f45d349e9
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-01-28 17:02:52 +0530

    commit

--- pr30.java
+++ pr30.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class pr30 {
     public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
         
+
+        sc.close();
     }
 }
