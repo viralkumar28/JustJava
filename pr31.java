3b7626050544355c155497e5b1354fa196095451
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-01-30 16:49:18 +0530

    commit

--- pr31.java
+++ pr31.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class pr31 {
     public static void main(String args[]) {
-        
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        fact(num);
+        in.close();
+    }
+    static void fact(int num){
+        int fact =1;
+        for(int i=1;i<=num;i++){
+           fact = fact*i;
+        }
+        System.out.println(fact);
     }
 }
