b6e2a8faa8b91f3b696edb560c8beced19ce6d9a
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-01-31 17:33:54 +0530

    commit

---
+++ pr33.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class pr33 {
+    public static void main(String args[]) {
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        hcf(n,m);
+        lcm(n,m);
+        sc.close();
+    }
+    static void hcf(int n,int m){
+
+    }
+    static void lcm(int n,int m){
+        
+    }
+}
