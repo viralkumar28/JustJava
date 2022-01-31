7f97237dabce40688dda89c45ffdadeaa48864f0
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-01-31 16:29:58 +0530

    commit

--- pr32.java
+++ pr32.java
@@ -8,6 +8,18 @@ public class pr32{
         sc.close();
     }
     static void palindrome(int num){
-         
+        int ans = 0;
+        int temp = num;
+        while(num>0){
+         int rem = num % 10;
+         num = num /10;
+         ans = ans * 10 + rem;
+        }
+        if(temp == ans){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not Palindrome");
+        }
+
     }
 }
