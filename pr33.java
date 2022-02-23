c892c4038c02ee1c7a3488986b961b9f37934f34
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-02-23 16:51:23 +0530

    commit

--- pr33.java
+++ pr33.java
@@ -4,15 +4,26 @@ public class pr33 {
         Scanner sc= new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        hcf(n,m);
+        System.out.println(hcf(n, m));
+        System.out.println(lcm(n,m));
         sc.close();
     }
-    static void hcf(int n,int m){
-        int num=0;
-        for(int i=1;i<=num;i++){
-            num = n/i;
-            
-        }        
-
+    static int hcf(int n,int m){
+    int hcf =1;
+        for(int i=1;i<=n && i<=m;i++){
+             if(n%i==0 && m%i==0){
+                 hcf = i;
+             }
+        }
+        return hcf;        
+    }
+    static int lcm(int n, int m){
+        int lcm=0;
+        for(int i=1;i<=n && i<=m;i++){
+            if(n%i==0 && m%i==0){
+             lcm=(n*m)/hcf(n, m);
+            }
+        }
+        return lcm;
     }
 }
