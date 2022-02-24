ee32c56730832ee1e68c0aacac2dfb4eff4e66d9
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-02-24 16:50:09 +0530

    commit

--- pr34.java
+++ pr34.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class pr34 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String st = new String();
         
+        sc.close();
     }
 }
