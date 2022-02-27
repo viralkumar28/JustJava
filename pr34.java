b9721388924dc1d72f0d39dce3f1588465a8cab7
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-02-27 17:33:08 +0530

    commit

--- pr34.java
+++ pr34.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 
 public class pr34 {
     public static void main(String[] args) {
-        char st;
-        for(st='A';st<='Z';st++){
-            System.out.println(st);
-        }
-    }
+        Scanner sc= new Scanner(System.in);
+      int a=sc.nextInt(),b=sc.nextInt();
+      prime(a,b);
+        sc.close();
+       }
+       public static void prime(int a,int b){
+            for(int i=a;i<=b;i++){
+                if(i%2!=0 && i%3!=0){
+                    System.out.println(i);
+                }
+        if(i/2==0)
+        System.out.println("Not prime");
+
+                
+            }
+
+       }
 }
