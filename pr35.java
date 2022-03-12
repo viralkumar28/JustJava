d42034458fe0981d7689d2816379c8f3a629367e
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-03-12 23:09:59 +0530

    commit

--- pr35.java
+++ pr35.java
@@ -4,13 +4,7 @@ public class pr35 {
     public static void main(String args[]){
         Scanner sc= new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n];
-        for(int i=0;i<n;i++){
-            arr[arr.length] = sc.nextInt();    
-        }
-        for(int i=0;i<n;i++){
-            System.out.println(arr[n]);
-        }
+        
         
         sc.close();
     }
