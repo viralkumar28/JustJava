774cad47369714f8d26ac25935714634487bbb32
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-05-14 23:11:54 +0530

    commit

--- pr36.java
+++ pr36.java
@@ -1,17 +1,34 @@
+import java.util.Scanner;
+
 public class pr36 {
     public static void main(String[] args) {
-        int r, sum=0, temp;
-        int n = 454;
-        temp = n;
-        while(n>0){
-            r= n%10;
-            sum=(sum*10)+r;
-            n=n/10;
+        // int r, sum=0, temp;
+        // int n = 454;
+        // temp = n;
+        // while(n>0){
+        //     r= n%10;
+        //     sum=(sum*10)+r;
+        //     n=n/10;
 
-        }
-        if(temp== sum)
-            System.out.println("palindrome");
-        else    
-            System.out.println("noy palindrome");
+        // }
+        // if(temp== sum)
+        //     System.out.println("palindrome");
+        // else    
+        //     System.out.println("noy palindrome");
+
+        String original , reverse="";
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the string: ");
+        original = sc.nextLine();
+        int length = original.length();
+        for(int i = length -1 ;i>=0;i--)
+            reverse = reverse + original.charAt(i);
+        if(original.equals(reverse))
+        System.out.println("palindrome");
+        else
+        System.out.println("not palindrome");
+        sc.close();
+
     }
 }
