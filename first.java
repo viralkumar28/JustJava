8e264f413872d84efedd5148183467bccb4666c5
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-07-17 11:21:14 +0530

    commit

--- first.java
+++
@@ -1,7 +0,0 @@
-
-class first{
-    public static void main() {
-        System.out.println("hello");
-    }
-}
-
--- pr37.java
+++
@@ -1,5 +0,0 @@
-public class pr37 {
-    public static void main(String[] args) {
-         //do java
-    }
-}
