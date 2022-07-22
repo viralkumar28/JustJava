c7b1227d0e7a8b7d2ab5b578e194083656fa28bb
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-07-22 15:56:10 +0530

    commit

--- prac1.java
+++ prac1.java
@@ -3,10 +3,13 @@ class prac1{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        if(num%2==0 || num%3==0){
-            System.out.println("not prime");
-        }else{
-            System.out.println("prime");
+        int c=2;
+        if(c<num){
+            if(num%c==0){
+                System.out.println("Not Prime");
+            }
+            c++;
+            
         }
 
         sc.close();
