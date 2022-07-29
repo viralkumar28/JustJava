a37ae20187e333c4c84374b2005f3264d3fec2f7
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-07-29 19:05:52 +0530

    commit

--- prac1.java
+++ prac1.java
@@ -2,20 +2,24 @@ import java.util.*;
 class prac1{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int c=2;
         boolean flag = false;
-        while(c<num){
-            if(num%c==0){
-                System.out.println("Not Prime");
-                break;
+        int c=2;
+        int num = sc.nextInt();
+        if(num ==0 || num == 1 ){
+            System.out.println("Not Prime");
+        }
+            while(c<num){
+                if(num%c==0){
+                    System.out.println("Not Prime");
+                    break;
+                }
+                    c++;
+                    flag = true;
             }
-                c++;
-                flag = true;
-        }
-        if(flag == true){
-            System.out.println("Prime");
-        }
+        
+            if(num==2 ||flag == true){
+                System.out.println("Prime");
+            }
         sc.close();
     }
 }
