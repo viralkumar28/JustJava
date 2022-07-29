79143a1982062d7a011c2b227c4038ed703130e8
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-07-29 19:11:56 +0530

    commit

---
+++ prac2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class prac2 {
+    public static void main(String[] args) {
+        int a=0,b=1,c;
+        Scanner sc = new Scanner (System.in);
+        int count = sc.nextInt();
+        System.out.println(a+" "+b);
+        for(int i=2;i<count;++i){
+            c=a+b;
+            System.out.println(" "+c);
+            a=b;
+            b=c;
+        }
+        sc.close();
+
+    }
+}
