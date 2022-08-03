407c4406edfbe2473ce3a76d4f8d2fac55dcc561
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-08-03 18:56:02 +0530

    commit

--- prac2.java
+++ prac2.java
@@ -1,18 +1,16 @@
 import java.util.*;
 public class prac2 {
     public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
         int a=0,b=1,c;
-        Scanner sc = new Scanner (System.in);
-        int count = sc.nextInt();
+        int num = sc.nextInt();
         System.out.println(a+" "+b);
-        for(int i=2;i<count;++i){
-            c=a+b;
-            System.out.println(" "+c);
-            a=b;
-            b=c;
+        for(int i=0;i<num;i++){
+            c = a+b;
+            System.out.println(c);
+            a = b;
+            b = c;
         }
-        sc.close();
-
-        
+        sc.close();   
     }
 }
