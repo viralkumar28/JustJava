344719275fab45908ce8161b9287bd7d942f99fd
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-08-03 19:01:36 +0530

    commit

--- prac3.java
+++ prac3.java
@@ -3,9 +3,7 @@ class prac3{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        switch(str){
-             
-        }
+        
         sc.close();
     }
 }
