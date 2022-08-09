0da90790c7c80a5302914570941bb65b7486c280
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-08-09 18:01:39 +0530

    commit

--- prac3.java
+++ prac3.java
@@ -1,13 +1,51 @@
+// import java.util.*;
+// class prac3{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//        if(n%2==0){
+//         System.out.println("even");
+//        }else{
+//         System.out.println("odd");
+//        }
+//         sc.close();
+//     }
+// }
+
+//Second
+// import java.util.*;
+// class prac3{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         String str = sc.next();
+//         System.out.println(str+" Viral");
+
+//         sc.close();
+//     }
+// }
+
+
+//Third
+// import java.util.*;
+// class prac3{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int p = sc.nextInt();
+//         int t = sc.nextInt();
+//         float r = sc.nextFloat();
+//         float si = (p*t*r)/100;
+//         System.out.println(si);
+
+//         sc.close();
+//     }
+// }
+
+//Fourth
 import java.util.*;
 class prac3{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-       if(n%2==0){
-        System.out.println("even");
-       }else{
-        System.out.println("odd");
-       }
+        Scanner sc= new Scanner(System.in);
+        
         sc.close();
     }
 }
