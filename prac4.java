a32cb715fa20f6e0bb416dccfdd94c300b4c64ac
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-08-16 18:59:42 +0530

    commit

--- prac4.java
+++ prac4.java
@@ -2,7 +2,10 @@ import java.util.*;
 public class prac4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int rupee = sc.nextInt();
+        int usd = 80;
+        int converted = usd * rupee;
+        System.out.println("Converted = "+converted);
         
         sc.close();
     }
