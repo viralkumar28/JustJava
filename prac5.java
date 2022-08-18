9189cc6370d9d13903fce5fd4f9ceeeaba78aa28
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-08-18 13:34:51 +0530

    commit

--- prac5.java
+++ prac5.java
@@ -1,5 +1,19 @@
+import java.util.*;
 public class prac5 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int num =12321;
+        int orgnum = num;
+        int count =0;
+        while(num>0){
+            int rem = num % 10;
+           num = num /10;
+            count = count*10+rem;
+        }
+        if(orgnum == count){
+            System.out.println("palindrome");
+        }else
+        System.out.println("not palindrome");
+        sc.close();
     }
 }
