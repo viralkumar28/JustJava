51d8aadd2d07e8c05c606cddf18ff0154d12c51d
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-08-19 13:25:49 +0530

    commit

--- prac4.java
+++ prac4.java
@@ -2,11 +2,12 @@ import java.util.*;
 public class prac4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int rupee = sc.nextInt();
-        int usd = 80;
-        int converted = usd * rupee;
-        System.out.println("Converted = "+converted);
-        
+        String str = sc.nextLine();
+        if(str.toUpperCase() != null ){
+            System.out.println(str.toLowerCase());
+        }else{
+            System.out.println(str);
+        }
         sc.close();
     }
 }
