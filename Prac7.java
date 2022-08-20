0219462ebe21a587270d935733f52fc0f6eebda4
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-08-20 13:03:47 +0530

    commit

---
+++ Prac7.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Prac7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = "viral";
+        String nstr = str.rev
+        sc.close();
+    }
+}
