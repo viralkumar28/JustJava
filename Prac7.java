e2a6c8d4b2870dd8762c28207807c789064a0f09
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-08-20 13:21:11 +0530

    Commit

--- Prac7.java
+++ Prac7.java
@@ -3,8 +3,12 @@ public class Prac7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num=sc.nextInt();
-        if(num ==0 || num ==1 ){
-            System.out.println("");
+        if(num==0 || num==1 ){
+            System.out.println("Not Prime");
+        }else if(num%2==0 && num%3==0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
         }
 
     sc.close();
