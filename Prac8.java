a984d51290e639d8644320f72e62076f129f5fb1
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-08-21 16:45:59 +0530

    Commit

--- Prac8.java
+++ Prac8.java
@@ -1,8 +1,22 @@
 import java.util.*;
-public class Prac8 {
+class Prac8{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc= new Scanner(System.in);
+        int num = sc.nextInt();
+        boolean temp = false;
         
+        for(int i=2;i<=num/2;++i){
+            if(num%i==0){
+                temp = true;
+            break;
+            }
+            
+        }
+        if(num ==0 || num ==1|| temp ==true){
+            System.out.println("Not Prime");
+        }else{
+            System.out.println("Prime");
+        }
         sc.close();
     }
 }
