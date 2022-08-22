714d7eec8313eb095c5501ab223ea0517be48d06
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-08-22 15:17:10 +0530

    Commit

--- Prac1.java
+++ Prac1.java
@@ -1,5 +1,19 @@
+import java.util.*;
 public class Prac1 {
     public static void main(String[] args) {
-        
+        Scanner sc= new Scanner(System.in);
+        String str = sc.nextLine();
+        String find = sc.nextLine();
+        findFrequency(str,find);
+        System.out.println();
+        sc.close();
+    }
+
+    private static void findFrequency(String str, String str1) {
+        if(str.contains(str1)){
+           System.out.println();
+        }else{
+            System.out.println("Word not found");
+        }
     }
 }
