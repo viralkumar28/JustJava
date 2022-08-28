22cade62d48436adcaeb77a56fe5f1f4a66543a9
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-08-28 11:11:33 +0530

    Commit

--- Prac1.java
+++ Prac1.java
@@ -1,9 +1,8 @@
-import java.time.temporal.ChronoUnit;
 import java.util.*;
 public class Prac1 {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-       String str= sc.nextLine().charAt(0);
+      
         sc.close();
     }
 }
