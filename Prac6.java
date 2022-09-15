50d5027ee5e4ac66f6f1cf379e5532258803f9ef
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-09-15 20:15:18 +0530

    Commit

--- Prac6.java
+++ Prac6.java
@@ -2,7 +2,12 @@ import java.util.*;
 public class Prac6 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-
+    char ch = sc.next().charAt(0);
+    if((ch >= 'a' && ch <= 'z')||( ch >='A' && ch <='Z')){
+        System.out.println("Alphabet");
+    }else{
+        System.out.println("Not Alphabet");
+    }
     sc.close();
   }  
 }
