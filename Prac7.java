20b0458d1cef4867ecc7eef8f24dd4846b4b69da
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-11-23 20:47:48 +0530

    Commit

--- Prac7.java
+++ Prac7.java
@@ -3,10 +3,18 @@ public class Prac7 {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         
-        int a=10, b=20;
-        System.out.println(a+b);
-
+        for(int i=0;i<10;i++){
+            sum();
+        }
+        
+         
 
         sc.close();
     }
+
+    static void sum(){
+        int a=10,b=20;
+        System.out.println(a+b);
+    }
+
 }
