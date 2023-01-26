f0bdeb4c3fa3096148b0137796c5cba834b58e97
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2023-01-26 13:39:20 +0530

    loops concept

--- loops.java
+++ loops.java
@@ -53,7 +53,45 @@ public class loops {
         //     System.out.println();
         // }
 
+        // int n = sc.nextInt();
+        // for(int i=n;i>=1;i--){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+        // int n = sc.nextInt();
+        // int num =1;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(num+" ");
+        //         num++;
+        //     }
+        //     System.out.println();
+        // }
+
+        // char ch = 'A';
+        // int n = sc.nextInt();
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(ch+" ");
+        //         ch++;
+        //     }
+        //     System.out.println();
+        // }
+
         int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print("1 ");
+                }else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
 
         sc.close(); 
     }
