efd1861f1faccdada7599a51a7aa906bc9dd2325
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2023-01-28 13:24:51 +0530

    Advance Patter added

--- loops.java
+++ loops.java
@@ -146,34 +146,48 @@ public class loops {
         // }
 
 
-        int n =sc.nextInt();
-        for(int i=n;i>=1;i--){
-            for(int j=1; j<=i;j++){
-                System.out.print("*");
-            }
-            int spaces = 2*(n-i);
-            for(int j=1;j<=spaces;j++){
-                System.out.print(" ");
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
+        // int n =sc.nextInt();
+        // for(int i=n;i>=1;i--){
+        //     for(int j=1; j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     int spaces = 2*(n-i);
+        //     for(int j=1;j<=spaces;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
 
+        //     }
+        //     System.out.println();
+        // }
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     int spaces = 2*(n-i);
+        //     for(int j=1;j<=spaces;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+            int n =sc.nextInt();
+            int m =sc.nextInt();
+            for(int i=1;i<=n;i++){
+                int spaces = n-i;
+                for(int j=1;j<=spaces;j++){
+                    System.out.print(" ");
+                }
+                for(int j=1;j<=m;j++){
+                    System.out.print("*");
+                }
+                System.out.println();
             }
-            System.out.println();
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            int spaces = 2*(n-i);
-            for(int j=1;j<=spaces;j++){
-                System.out.print(" ");
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+
 
         sc.close(); 
     }
