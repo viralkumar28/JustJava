6298532bd75b4ea73a05251c0748c5d9cdcdbee1
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2023-02-19 18:20:21 +0530

    commit

--- arrays.java
+++ arrays.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class arrays {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arry = new int[n];
+        for(int i=0;i<n;i++){
+           arry[i] = sc.nextInt();
+        }
+        int x = sc.nextInt();
+        for(int i=0;i<n;i++){
+            if(arry[i] == x){
+                System.out.println("X found at "+i);
+            }
+        }
+       
+        sc.close();
     }
 }
