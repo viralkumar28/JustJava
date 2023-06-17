c919a519246085eaaad2a05a799adfd61a0c0691
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2023-06-17 16:37:23 +0530

    new file added

--- secondlargest.java
+++ secondlargest.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class secondlargest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        String lastname = sc.next();
-        String fullname = name+" "+lastname;
-        System.out.println(fullname.length());
+        int i = sc.nextInt();
+        String name[] = new String[i]; 
+     
+        
         
         sc.close();
         }
