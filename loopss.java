907d684ec2090a3f944b36814c2431af6d3ee9ed
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2023-07-02 16:24:28 +0530

    second time pattern programs

---
+++ loopss.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class loopss {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        // from this link you can  get all the patters program https://takeuforward.org/strivers-a2z-dsa-course/must-do-pattern-problems-before-starting-dsa/
+
+        int n = sc.nextInt();
+//        for(int i=1;i<=n;i++){
+//            for(int j=1;j<=i;j++){
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
+    }
+}
