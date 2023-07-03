1b554e1883d66a4c4b04e4b801b26d941ba99154
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2023-07-03 15:41:18 +0530

    commit

--- loopss.java
+++ loopss.java
@@ -7,11 +7,11 @@ public class loopss {
         // from this link you can  get all the patters program https://takeuforward.org/strivers-a2z-dsa-course/must-do-pattern-problems-before-starting-dsa/
 
         int n = sc.nextInt();
-//        for(int i=1;i<=n;i++){
-//            for(int j=1;j<=i;j++){
-//                System.out.print(j);
-//            }
-//            System.out.println();
-//        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
     }
 }
