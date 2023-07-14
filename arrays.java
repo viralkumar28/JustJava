d61108f5f328f0ec6b78cdf7fa213b0e3c705dd4
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2023-07-14 16:14:21 +0530

    commit

--- arrays.java
+++ arrays.java
@@ -3,18 +3,13 @@ import java.util.Scanner;
 public class arrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arry = new int[n];
-        for(int i=0;i<n;i++){
-           arry[i] = sc.nextInt();
-        }
-        int x = sc.nextInt();
-        for(int i=0;i<n;i++){
-            if(arry[i] == x){
-                System.out.println("X found at "+i);
-            }
-        }
-       
+       int[] marks = new int[3];
+       marks[0] = 97;
+       marks[1] = 98;
+       marks[2] = 96;
+       for(int i=0;i<3;i++){
+           System.out.println(marks[i]);
+       }
         sc.close();
     }
 }
